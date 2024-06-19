f36fb2f0b1d82e446b25f8452bd23deafad60a33
Charles Raj <charles.raj412@gmail.com>
2024-06-19 15:23:09 -0400

    Initial Commit

---
+++ YoutubeSwiftUi/YoutubeSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeSwiftUiApp.swift
+//  YoutubeSwiftUi
+//
+//  Created by charles raj on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
